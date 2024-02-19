2082283035657cf085ebad25e9024753a6f080bb
admin <spodaiko@lytvynov-production.com>
2024-02-19 14:45:10 +0200

    Fix banner

--- components/CookieBanner/CookieBanner.tsx
+++ components/CookieBanner/CookieBanner.tsx
@@ -26,7 +26,7 @@ export default function CookieBanner() {
 
   return (
     <div
-      className={`my-10 mx-auto max-w-max md:max-w-screen-sm
+      className={`z-50 my-10 mx-auto max-w-max md:max-w-screen-sm
                         fixed bottom-0 left-0 right-0 
                         flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
                          bg-gray-700 rounded-lg shadow,  ${cookieConsent != null ? 'hidden' : 'flex'} `}
--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -12,7 +12,7 @@ const About = () => (
     />
     <Header />
     <div className='mb-[100px] z-20'>
-      <h1 className='px-3 lg:mx-0 text-center w-full mx-auto text-center text-[22px] sm:text-[26px] lg:text-[30px]'>
+      <h1 className='px-3 lg:mx-0 text-center w-full mx-auto text-[22px] sm:text-[26px] lg:text-[30px]'>
         Тут ви знайдете найкращі зібрані біблійні фільми про Ісуса Христа. Проповіді пастерів, священників, людей та
         багато іншого.
       </h1>
